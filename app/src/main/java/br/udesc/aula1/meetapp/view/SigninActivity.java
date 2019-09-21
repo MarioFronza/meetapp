1cb98ec83250ad4535d3829baf43fc664846115e
Mario <mariofronza1@gmail.com>
2019-09-21 07:16:59 -0300

    feat: Singin and Signup pages

--- app/src/main/java/br/udesc/aula1/meetapp/view/SigninActivity.java
+++ app/src/main/java/br/udesc/aula1/meetapp/view/SigninActivity.java
@@ -1,4 +1,4 @@
-package br.udesc.aula1.meetapp;
+package br.udesc.aula1.meetapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,23 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import br.udesc.aula1.meetapp.view.SigninActivity;
+import br.udesc.aula1.meetapp.MainActivity;
+import br.udesc.aula1.meetapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity {
 
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
 
-        button = findViewById(R.id.buttonTest);
+        button = findViewById(R.id.signinButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SigninActivity.class));
+                startActivity(new Intent(SigninActivity.this, SignupActivity.class));
             }
         });
     }
